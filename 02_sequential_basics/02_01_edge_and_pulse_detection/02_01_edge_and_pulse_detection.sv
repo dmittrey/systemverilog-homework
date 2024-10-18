c53ef6f8d7fe751d657eb388e56b42cf569c4fe8
d.zubakhin <dmitryzubakhin@mail.ru>
2024-10-18 08:00:27 +0300

    2-hw: Add 01_pulse_detection

--- 02_sequential_basics/02_01_edge_and_pulse_detection/02_01_edge_and_pulse_detection.sv
+++ 02_sequential_basics/02_01_edge_and_pulse_detection/02_01_edge_and_pulse_detection.sv
@@ -26,11 +26,41 @@ endmodule
 
 module one_cycle_pulse_detector (input clk, rst, a, output detected);
 
-  // Task:
-  // Create an one cycle pulse (010) detector.
-  //
-  // Note:
-  // See the testbench for the output format ($display task).
+  typedef enum bit [1:0]
+  {
+    S0 = 2'd0,
+    S1 = 2'd1,
+    S2 = 2'd2
+  } 
+  state_e;
+  
+  state_e state, next_state;
 
+  // State register
+
+  always_ff @ (posedge clk)
+    if (rst)
+      state <= S0;
+    else
+      state <= next_state;
+
+  // Next state logic
+
+  always_comb
+  begin
+      next_state = state;
+
+      case (state)
+      S0: if (~ a) next_state = S1;
+      S1: if (  a) 
+            next_state = S2;
+      // Не задерживаемся во состоянии, тк у нас автомат Милли, 
+          // то по выходному можно определить да/нет 
+      S2: next_state = S0;
+      endcase
+  end
+  
+  // Output logic based on current state
+  assign detected = ((~ a) & state == S2);
 
 endmodule
